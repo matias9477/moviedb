6de61620d468835d41157dcc86386a5eb7e7f10b
Matias <matias.turra@gmail.com>
2022-02-08 20:57:58 -0300

    Fully functional search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
@@ -10,13 +14,13 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () =>{
 
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm, searchTerm } = useHomeFetch();
 
     console.log(state);
 
     return (
         <>
-        {state.results[0] ? 
+        { !searchTerm && state.results[0] ? 
             <HeroImage 
                 image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                 tittle={state.results[0].original_title}
@@ -24,8 +28,22 @@ const Home = () =>{
             />
             :null 
         }
+        <SearchBar setSearchTerm={setSearchTerm} />
+        <Grid header={ searchTerm ? 'Search Result' : 'Popular Movies'}>
+            {state.results.map(movie => (
+                <Thumb 
+                    key={movie.id}
+                    clickable
+                    image={
+                        movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage
+                    }
+                    movieId={movie.id}
+                />
+            ))}
+        </Grid>
+        <Spinner/>
         </>
-    ) 
+    );
     
 };
 
